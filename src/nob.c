9c119b9e647d72fc96f5f75fcbf83d9f608b24d9
Zb1ndev <zbindev@gmail.com>
2024-10-08 09:50:27 -0500

    Initial Commit

---
+++ src/nob.c
@@ -0,0 +1,126 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "nob.h"
+
+// #define DEBUG
+
+#ifdef DEBUG
+    #define NOBFILE "/testfile"
+#else
+    #define NOBFILE "/nob"
+#endif
+
+char* GetBuildTemplate(char* _tag, char* _workingPath) {
+
+    char* _fileDir = malloc(strlen(NOB_getcwd()) + 9);
+    sprintf(_fileDir, "%s%s", NOB_getcwd(), NOBFILE);
+    
+    NOB_lines _lines;
+    NOB_fgetl(&_lines, _fileDir);
+
+    size_t _templateIndex = 0;
+    NOB_strkvp* _templates = malloc(sizeof(NOB_strkvp) * _lines.maxIndex);
+
+    for (size_t l = 0; l < _lines.maxIndex; l++) {
+        if (strchr(_lines.content[l].content, '#') == NULL && strchr(_lines.content[l].content, ':') != NULL) {
+            char* _buffer = strchr(_lines.content[l].content,':');
+            _templates[_templateIndex].value = malloc(strlen(_buffer));
+            memcpy(_templates[_templateIndex].value, _buffer+1, strlen(_buffer));
+            size_t _keyIndex = (strlen(_lines.content[l].content) - strlen(_templates[_templateIndex].value));
+            _templates[_templateIndex].key = malloc(_keyIndex + 1); 
+            memcpy(_templates[_templateIndex].key, _lines.content[l].content, _keyIndex-1); 
+            _templates[_templateIndex++].key[_keyIndex-1] = '\0';
+        }
+    }
+    for (size_t t = 0; t < _templateIndex; t++) {
+        if (strcmp(_tag, NOB_remchr(_templates[t].key, ' ')) == 0) { 
+            return _templates[t].value; 
+        }
+    }
+
+    NOB_assert(1,"[ERROR] No Command with the provided tag was found.");
+
+}
+NOB_macros GetMacros(char* _workingPath) {
+
+    char* _fileDir = malloc(strlen(NOB_getcwd()) + 9);
+    sprintf(_fileDir, "%s%s", NOB_getcwd(), NOBFILE);
+    
+    NOB_lines _lines;
+    NOB_fgetl(&_lines, _fileDir);
+
+    NOB_macros _macros;
+    size_t _macroIndex = 0;
+    _macros.content = malloc(_lines.maxIndex * sizeof(NOB_strkvp));
+
+    for (size_t l = 0; l < _lines.maxIndex; l++) {
+        if (strchr(_lines.content[l].content, '#') != NULL) {
+            char* _macro = strchr(_lines.content[l].content, '#');
+            char* _buffer = strchr(_macro,':');
+            _macros.content[_macroIndex].value = malloc(strlen(_macro));
+            memcpy(_macros.content[_macroIndex].value,_buffer+1, strlen(_buffer));
+            size_t _keyIndex = (strlen(_macro) - strlen(_buffer));
+            _macros.content[_macroIndex].key = malloc(_keyIndex + 1);
+            memcpy(_macros.content[_macroIndex].key, _macro+1, _keyIndex-1);
+            _macros.content[_macroIndex++].key[_keyIndex-1] = '\0';
+        }
+    }
+
+    _macros.maxIndex = _macroIndex;
+
+    return _macros;
+
+}
+char* GenerateCommand(char* _template, NOB_macros _macros) {
+
+    size_t _size = strlen(_template) + 200; // Find Actual Largest Posible Size;
+    char* _command = malloc(_size); 
+    memset(_command, 0, _size);
+
+    __uint8_t _skip = 0;
+    char* _buffer = strtok(_template, " ");
+    while (_buffer != NULL) {
+        
+        _buffer = NOB_remchr(_buffer, '\n');
+
+        for (size_t i = 0; i < _macros.maxIndex; i++) {
+            if (strcmp(_buffer, NOB_remchr(_macros.content[i].key, ' ')) == 0) {
+                sprintf(_command, "%s %s", _command, NOB_remchr(_macros.content[i].value, '\n'));
+                _skip++;
+                break;
+            }
+        }
+
+        if (_skip == 0)
+            sprintf(_command, "%s %s", _command, _buffer);
+
+        _buffer = strtok(NULL, " ");
+        _skip = 0;
+    }
+
+    return _command;
+
+}
+
+int main(int argc, char** argv) {
+
+    #ifdef DEBUG
+
+        char* _template = GetBuildTemplate("build", NOB_getcwd());
+        char* _command = GenerateCommand(_template, GetMacros(NOB_getcwd()));
+        printf("\n%s", _command);
+
+    #else
+    
+        NOB_assert((argv[1] == NULL),"[ERROR] Please Enter and Argument. \n");
+        char* _template = GetBuildTemplate(argv[1], NOB_getcwd());
+        char* _command = GenerateCommand(_template, GetMacros(NOB_getcwd()));
+        NOB_assert((_command == NULL),"[ERROR] Generated Command was Invalid. \n");
+        system(_command);
+
+    #endif
+
+    return 0;
+
+}
---
+++ src/nob.h
@@ -0,0 +1,119 @@
+#ifndef NOB_H
+#define NOB_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct NOB_strkvp {
+    char* key;
+    char* value; 
+} NOB_strkvp;
+typedef struct NOB_macros {
+    NOB_strkvp* content;
+    size_t maxIndex; 
+} NOB_macros;
+typedef struct NOB_line {
+    size_t index;
+    char* content;
+} NOB_line;
+typedef struct NOB_lines {
+    size_t maxIndex;
+    NOB_line* content;
+} NOB_lines;
+
+#pragma region NOB_getcwd
+    #ifdef WINDOWS
+        #include <direct.h>
+        /* Returns the full path of your working directory. */
+        char* NOB_getcwd() {
+            char* _buf;
+            _buf = malloc(_buf, sizeof(char) * 256);
+            if (_getcwd(_buf, sizeof(_buf)) != NULL)
+                return _buf;
+            return NULL;
+        }
+    #else
+        #include <unistd.h>
+        /* Returns the full path of your working directory. */
+        char* NOB_getcwd() { 
+            char* _currentDir = getcwd(NULL, 0);
+            return _currentDir;
+        }
+    #endif
+#pragma endregion
+#define NOB_assert(expression, message) if(expression) { printf(message); exit(1); }
+#define NOB_strmov(movstrdest, movstrsrc) \
+    movstrdest = malloc(strlen(movstrsrc)+1); \
+    memcpy(movstrdest, movstrsrc, strlen(movstrsrc)+1);
+#define NOB_strclr(strclrsrc) memset(strclrsrc, 0, strlen(strclrsrc));
+
+size_t NOB_carrsize(char** _arr) {
+    size_t _length = 0;
+    while (_arr[_length++] != NULL);
+    return _length-1;
+}
+size_t NOB_fsizeof(char* _fileDir) {
+
+    FILE* _file = fopen(_fileDir, "r");
+    NOB_assert((_file == NULL),"[ERROR] File Not Found. \n");
+  
+    fseek(_file, 0L, SEEK_END); 
+    size_t _length = ftell(_file); 
+
+    fclose(_file);
+    return _length;
+    
+}
+void NOB_fgetl(NOB_lines* _dest, char* _fileDir) {
+
+    FILE* _file = fopen(_fileDir, "r");
+    NOB_assert((_file == NULL),"[ERROR] File Not Found. \n");
+
+    size_t _index = 0;
+    size_t _size = NOB_fsizeof(_fileDir) + 1;
+    char* _buffer = malloc(_size);
+    _dest->content = malloc(_size * sizeof(NOB_line));
+
+    NOB_assert((_size <= 1), "[ERROR] File is Empty.");
+
+    while (fgets(_buffer, _size, _file) != NULL) {
+        NOB_strmov(_dest->content[_index].content, _buffer);
+        _dest->content->index = _index++;
+    }
+    _dest->maxIndex = _index;
+        
+    free(_buffer);
+    fclose(_file);
+
+}
+char* NOB_remchr(char* _src, char _delimiter) {
+
+    char* _ret; 
+    NOB_strmov(_ret, _src);
+    
+    char* _endbuffer = malloc(strlen(_src) + 1);
+    char* _startbuffer = malloc(strlen(_src) + 1);
+    size_t _stringLength = 0;
+
+    for (size_t i = 0; i < (strlen(_src) + 1); i++){
+        if (strchr(_ret, _delimiter) != NULL) {
+            
+            NOB_strclr(_endbuffer);
+            NOB_strclr(_startbuffer);
+
+            size_t _index = -(strlen(strchr(_ret, _delimiter)) - strlen(_ret));
+            memcpy(_endbuffer, _ret+_index+1, strlen(_ret) + 1);
+            memcpy(_startbuffer, _ret, _index);
+            sprintf(_ret, "%s%s", _startbuffer, _endbuffer);
+            _stringLength++;
+
+        }
+    }
+
+    _ret[(strlen(_src)-1) - (_stringLength-1)] = '\0';
+    return _ret;
+
+}
+
+#endif
